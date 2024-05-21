bd985adfa71b451d92ca6b2cd7e89db2cad73778
Kuu1208 <130426876+Kuu1208@users.noreply.github.com>
2024-05-21 20:38:53 +0900

    BookDetail fetch 수정

--- src/components/BookDetails/BookDetail.jsx
+++ src/components/BookDetails/BookDetail.jsx
@@ -1,38 +1,49 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const BookDetail = () => {
-    const { id } = useParams(); // URL에서 베스트셀러 ID를 가져옴
-    const [bookDetail, setBookDetail] = useState(null);
+    const { isbn } = useParams();
+    const [book, setBook] = useState(null);
 
     useEffect(() => {
-        fetch(`http://43.201.231.40:8080/open/bestseller`, {
+        const apiUrl = `http://43.201.231.40:8080/open/search/book/${isbn}`;
+        console.log('요청 보낸 URL:', apiUrl);
+
+        fetch(apiUrl, {
             method: 'GET',
             headers: {
                 'Accept': '*/*'
             }
         })
-            .then(response => response.json())
-            .then(data => {
-                setBookDetail(data); 
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('서버 응답이 올바르지 않습니다.');
+                }
+                return response.json();
             })
-            .catch(error => console.error('Error fetching book detail:', error));
-    }, [id]); 
-    
+            .then(data => {
+                console.log('책 정보:', data);
+                if (data && data.item && data.item.length > 0) {
+                    setBook(data.item[0]); 
+                } else {
+                    throw new Error('책 정보가 없습니다.');
+                }
+            })
+            .catch(error => console.error('책 정보를 가져오는 중 오류 발생:', error));
+    }, [isbn]);
+
+    if (!book) {
+        console.log('책 정보가 아직 로드되지 않았습니다.');
+        return null;
+    }
+
+    console.log('책 정보:', book);
+
     return (
         <div>
-            {bookDetail ? (
-                <div>
-                    <h5>{bookDetail.title}</h5>
-                    <p>{bookDetail.author}</p>
-                    <p>{bookDetail.link}</p>
-                    <p>{bookDetail.cover}</p>
-                </div>
-            ) : (
-                <p>Loading...</p>
-            )}
+            <h1>{book.title}</h1>
         </div>
     );
-}
+};
 
 export default BookDetail;

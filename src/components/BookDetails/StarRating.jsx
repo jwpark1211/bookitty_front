73719094895d7a8a9007fb3d46449a5e117d891b
Kuu1208 <130426876+Kuu1208@users.noreply.github.com>
2024-05-26 16:19:51 +0900

    fix : color modification of stars

--- src/components/BookDetails/StarRating.jsx
+++ src/components/BookDetails/StarRating.jsx
@@ -2,38 +2,44 @@ import React, { useState } from 'react';
 import './StarRating.css';
 import LoginModal from './LoginModal';
 
-const StarRating = ({ value, setValue, onRatingSubmit, isSignedIn }) => {
-  const [showLoginModal, setShowLoginModal] = useState(false);
-  const stars = [1, 2, 3, 4, 5];
+const StarRating = ({ value, setValue, onRatingSubmit, onDeleteRating, isSignedIn }) => {
+    const [showLoginModal, setShowLoginModal] = useState(false);
+    const stars = [1, 2, 3, 4, 5];
 
-  const handleChange = (event) => {
-    const newValue = Number(event.target.value);
-    if (isSignedIn) {
-      setValue(newValue);
-      onRatingSubmit(newValue);
-    } else {
-      setShowLoginModal(true);
-    }
+    const handleChange = (event, star) => {
+      const newValue = Number(event.target.value);
+      if (isSignedIn) {
+          if (newValue === value) {
+              onDeleteRating(newValue);
+          } else {
+              setValue(star);
+              onRatingSubmit(star);
+          }
+      } else {
+          setShowLoginModal(true);
+      }
   };
+  
+  
 
-  return (
-    <div className="star-rating">
-      {stars.map((star) => (
-        <React.Fragment key={star}>
-          <input
-            type="radio"
-            id={`star${star}`}
-            name="rating"
-            value={star}
-            checked={star === value}
-            onChange={handleChange}
-          />
-          <label htmlFor={`star${star}`} className={star <= value ? 'filled' : ''}></label>
-        </React.Fragment>
-      ))}
-      {showLoginModal && <LoginModal onClose={() => setShowLoginModal(false)} />}
-    </div>
-  );
+    return (
+        <div className="star-rating">
+            {stars.map((star) => (
+                <React.Fragment key={star}>
+                    <input
+                        type="radio"
+                        id={`star${star}`}
+                        name="rating"
+                        value={star}
+                        checked={star === value}
+                        onChange={(event) => handleChange(event, star)}
+                    />
+                    <label htmlFor={`star${star}`} className={star <= value ? 'filled' : ''}></label>
+                </React.Fragment>
+            ))}
+            {showLoginModal && <LoginModal onClose={() => setShowLoginModal(false)} />}
+        </div>
+    );
 };
 
 export default StarRating;

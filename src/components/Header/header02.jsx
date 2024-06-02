60e7d9bc89bf01c9815b481843830365d2073b47
Kuu1208 <130426876+Kuu1208@users.noreply.github.com>
2024-06-02 21:29:41 +0900

    fix : save name to session

--- src/components/Header/header02.jsx
+++ src/components/Header/header02.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './header02.css';
 
-const Header02 = ({ isSignedIn, name, profileImage }) => {
-  console.log("이름 : ", name);
+const Header02 = () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [name, setName] = useState('');
+  const [profileImage, setProfileImage] = useState('');
+
+  useEffect(() => {
+    const loginStatus = sessionStorage.getItem('login');
+    if (loginStatus) {
+      setIsSignedIn(true);
+      setName(sessionStorage.getItem('name') || '');
+      setProfileImage(sessionStorage.getItem('profileImg') || '');
+    }
+  }, []);
+
   return (
     <header className='header02'>
       <div className="logo-container">
@@ -13,7 +25,7 @@ const Header02 = ({ isSignedIn, name, profileImage }) => {
         </Link>
       </div>
       <div className="user-container">
-        {isSignedIn && (
+        {isSignedIn && name && profileImage && (
           <div className="user-profile">
             <img src={profileImage} alt="프로필 사진" className="profile-pic" width="30" height="30" />
             <Link to="/mypage" className="user-nickname">
